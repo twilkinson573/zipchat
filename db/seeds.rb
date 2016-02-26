1ed7376ae2deb2d6070e93ba546e5aa916a86fcb
ThomasMAC <tuds_wilkinson@hotmail.com>
2016-02-26 15:29:13 -0300

    Add name to User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email:'thomas@cool.com', password:'zipchatpassword')
-User.create(email:'tdog@awesome.com', password:'zipchatpassword')
-User.create(email:'money@booyah.com', password:'zipchatpassword')
-User.create(email:'satan@adventuretime.com', password:'zipchatpassword')
+User.delete_all
 
+u1 = User.create(email:'thomas@cool.com', password:'zipchatpassword',
+            first_name:'Thomas', surname:'Wilkinson')
+u2 = User.create(email:'tdog@awesome.com', password:'zipchatpassword',
+            first_name:'Finn', surname:'Human')
+u3 = User.create(email:'money@booyah.com', password:'zipchatpassword',
+            first_name:'Jake', surname:'Dog')
+u4 = User.create(email:'satan@adventuretime.com', password:'zipchatpassword',
+            first_name:'Princess', surname:'Bubblegum')
+
+puts 'Added 4 new users'
+
+Conversation.delete_all
+
+c1 = Conversation.create(sender_id: u1.id, recipient_id: u2.id)
+c2 = Conversation.create(sender_id: u2.id, recipient_id: u3.id)
+
+puts 'Added 2 new conversations'
+
+c1.messages.build(user_id: u1.id, body: 'Wanna go on an adventure?').save!
+c1.messages.build(user_id: u2.id, body: "Sounds good, I'll ask Jake too").save!
+
+c2.messages.build(user_id: u2.id, body: 'You are my dog!').save!
+c2.messages.build(user_id: u3.id, body: 'Noooo - I am your best friend! :)').save!
+
+puts 'Added seed messages to conversations'
