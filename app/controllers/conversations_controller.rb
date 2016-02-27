2c3e5fc042debf1bd97cd4600367bceb7c4b7daf
ThomasMAC <tuds_wilkinson@hotmail.com>
2016-02-26 21:00:04 -0300

    Add new conversation button

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,10 +2,16 @@ class ConversationsController < ApplicationController
   def index
     @conversations = Conversation.where('sender_id= ? OR recipient_id= ?', current_user.id, current_user.id)
     @users = User.all
+    @sender_id = current_user.id
+
+    @conversation = Conversation.new
   end
   
   def create
-    @conversation = set_or_create_conversation(params[:sender_id], params[:recipient_id])
+    @sender_id = current_user.id
+    @recipient_id = User.find(params[:conversation][:recipient_id])
+
+    @conversation = set_or_create_conversation(@sender_id, @recipient_id)
     redirect_to conversation_messages_path(@conversation)
   end
 
@@ -15,7 +21,7 @@ class ConversationsController < ApplicationController
   end
 
   def set_or_create_conversation(sender_id, recipient_id)
-    Conversation.between(sender_id, recipient_id).first ||= Conversation.create!(conversation_params)
+    Conversation.between(sender_id, recipient_id).first ||= Conversation.create!(sender_id: sender_id, recipient_id: recipient_id.id)
   end
 
 end
